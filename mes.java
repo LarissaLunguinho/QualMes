e0692a22bc9a44b09209206131c258eaa084a0e8
Larissa-Lunguinho <llunguinho16@gmail.com>
2021-06-26 20:39:31 -0300

    add mes

---
+++ mes.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class mes {
+
+	public static void main(String[] args) {
+		int mes;
+		Scanner entrada = new Scanner(System.in);
+		
+		System.out.println("Digite um mês:");
+		mes = entrada.nextInt();
+		
+		exibirMes(mes);
+	}
+		static void exibirMes (int mes) {
+			String texto ="";
+			switch (mes) {
+			case 1:
+				texto = "Janeiro";
+				break;
+			case 2:
+				texto = "Fevereiro";
+				break;
+			case 3:
+				texto = "Março";
+				break;
+			case 4:
+				texto = "Abril";
+				break;
+			case 5:
+				texto = "Maio";
+				break;
+			case 6:
+				texto = "Junho";
+				break;
+			case 7:
+				texto = "Julho";
+				break;
+			case 8:
+				texto = "Agosto";
+				break;
+			case 9:
+				texto = "Setembro";
+				break;
+			case 10:
+				texto = "Outubro";
+				break;
+			case 11:
+				texto = "Novembro";
+				break;
+			case 12:
+				texto = "Dezembro";
+				break;
+			case 13:
+				texto = "Mês inválido";
+				break; 		}
+			System.out.println("O mês corresponde á " + texto);
+		}
+
+}
